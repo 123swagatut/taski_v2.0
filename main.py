b9f793de4be5ab24f93041907c1bb35d8474276a
123swagatut <nobeamers@gmail.com>
2026-04-25 04:42:51 +0700

    Добавлена функция добавления задачи

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-.
+def add_task(tasks):
+    name = input("Введите описание задачи: ")
+    priority = input("Введите приоритет (высокий/средний/низкий): ")
+    tasks.append({"task": name, "priority": priority})
+    print("Задача добавлена!")
