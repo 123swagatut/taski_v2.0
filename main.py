f14eed420a435c2786a845f7eedd6baaac710096
123swagatut <nobeamers@gmail.com>
2026-04-25 04:44:07 +0700

    Добавлена функция поиска задачи

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-.
+def find_task(tasks):
+    query = input("Что ищем? ").lower()
+    found = [t for t in tasks if query in t['task'].lower()]
+    if found:
+        print("\nРезультаты поиска:")
+        for t in found:
+            print(f"- {t['task']} (Приоритет: {t['priority']})")
+    else:
+        print("Ничего не найдено.")
