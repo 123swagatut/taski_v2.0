9ff65e454a7745a3de6116e7eb6b823c63f6f8e3
123swagatut <nobeamers@gmail.com>
2026-04-25 04:43:31 +0700

    Добавлена функция удаления задачи

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-.
+def remove_task(tasks):
+    show_tasks(tasks)
+    try:
+        index = int(input("Введите номер задачи для удаления: ")) - 1
+        if 0 <= index < len(tasks):
+            removed = tasks.pop(index)
+            print(f"Задача '{removed['task']}' удалена.")
+        else:
+            print("Некорректный номер.")
+    except ValueError:
+        print("Ошибка: введите число.")
