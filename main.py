c5fe1468e3095b17648cb4044988d8604f640ac3
123swagatut <nobeamers@gmail.com>
2026-04-25 04:02:45 +0700

    Добавлена функция отображения задач

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-.
+def show_tasks(tasks):
+    if not tasks:
+        print("\nСписок задач пуст.")
+    else:
+        print("\n--- Текущие задачи ---")
+        for i, item in enumerate(tasks, 1):
+            print(f"{i}. {item['task']} [Приоритет: {item['priority']}]")
